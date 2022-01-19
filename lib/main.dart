1321c3b681314e4d9af63ed1b3b6c9c226532bff
felixx97 <abrahammfelixx@gmail.com>
2022-01-19 13:53:37 -0300

    interação button

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,56 @@
 import 'package:flutter/material.dart';
 
-class MyButton extends StatelessWidget {
-  const MyButton({Key? key}) : super(key: key);
+class CounterDisplay extends StatelessWidget {
+  const CounterDisplay({required this.count, Key? key}) : super(key: key);
+
+  final int count;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('MyButton was tapped!');
-      },
-      child: Container(
-        height: 50.0,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5.0),
-          color: Colors.lightGreen[600],
-        ),
-        child: const Center(
-          child: Text('Engage'),
-        ),
-      ),
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  const CounterIncrementor({required this.onPressed, Key? key})
+      : super(key: key);
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: const Text('Increment'),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({Key? key}) : super(key: key);
+
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CounterIncrementor(onPressed: _increment),
+        const SizedBox(width: 16),
+        CounterDisplay(count: _counter),
+      ],
     );
   }
 }
@@ -30,7 +60,7 @@ void main() {
     const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: MyButton(),
+          child: Counter(),
         ),
       ),
     ),
